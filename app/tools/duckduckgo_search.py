70f256f8cbd1bcee9538b400476c12046e09d456
tkong <tony.kong@redhat.com>
2024-02-16 18:32:29 +1300

    temp work

---
+++ app/tools/duckduckgo_search.py
@@ -0,0 +1,15 @@
+from langchain_community.tools import DuckDuckGoSearchResults
+from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
+
+
+from langchain_core.tools import tool
+
+
+@tool()
+def duckduckgo_search():
+    """
+    Get search result from Duckduckgo
+    """
+    wrapper = DuckDuckGoSearchAPIWrapper(max_results=1)
+    search = DuckDuckGoSearchResults(api_wrapper=wrapper)
+    return search
