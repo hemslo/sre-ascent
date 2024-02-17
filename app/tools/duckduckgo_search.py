9b6d12672187aea9e47bec9cd4837ac27f5ca8e4
Di Wang <dwan@redhat.com>
2024-02-17 22:06:29 +1100

    Fix duckduckgo_search
    Signed-off-by: Di Wang <dwan@redhat.com>

--- app/tools/duckduckgo_search.py
+++ app/tools/duckduckgo_search.py
@@ -1,14 +1,4 @@
 from langchain_community.tools import DuckDuckGoSearchResults
 from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 
-from langchain_core.tools import tool
-
-
-@tool()
-def duckduckgo_search():
-    """
-    Get search result from Duckduckgo
-    """
-    wrapper = DuckDuckGoSearchAPIWrapper(max_results=10)
-    search = DuckDuckGoSearchResults(api_wrapper=wrapper)
-    return search
+duckduckgo_search = DuckDuckGoSearchResults(api_wrapper=DuckDuckGoSearchAPIWrapper(max_results=10))
