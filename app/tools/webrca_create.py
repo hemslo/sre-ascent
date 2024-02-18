d86e35b884e1be5d103daf2af07d3fb92577a56e
Samantha Jayasinghe <sjayasin@redhat.com>
2024-02-18 16:31:01 +1300

    add incident description

--- app/tools/webrca_create.py
+++ app/tools/webrca_create.py
@@ -2,19 +2,22 @@ from langchain_core.tools import tool
 import requests
 
 @tool()
-def webrca_create() -> str:
+def webrca_create(description: str) -> str:
     """
     create webrca incident
     """
-
     url = "https://api.openshift.com/api/web-rca/v1/incidents"
     payload = {
-        "description": "test incident"
+        "description": description
     }
     headers = {"Content-Type": "application/json"}
-    response = requests.post(url, json=payload, headers=headers)
+    failedMessage = "We can’t create it. You can try to use `/rca new {incident-name}` for creating the incident"
+    try:    
+        response = requests.post(url, json=payload, headers=headers)
 
-    if response.status_code == 200:
-        return "incident created"
-    else:
-        return "incident failed"
+        if response.status_code == 200:
+            return "Created the incident"
+        else:
+            return failedMessage
+    except Exception as e:
+        return failedMessage
